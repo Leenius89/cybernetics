ae25466fafd16ed1e955a2f51ef37a30f2049010
Leenius89 <135181688+Leenius89@users.noreply.github.com>
2024-10-17 04:55:07 +0900

    6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,6 @@ module.exports = {
   extends: ['react-app'],
   rules: {
     'import/first': 'off',
-    'import/order': ['error', {
-      'groups': ['builtin', 'external', 'internal'],
-      'pathGroups': [
-        {
-          pattern: 'react',
-          group: 'external',
-          position: 'before'
-        }
-      ],
-      'pathGroupsExcludedImportTypes': ['react'],
-      'newlines-between': 'always',
-      'alphabetize': {
-        order: 'asc',
-        caseInsensitive: true
-      }
-    }]
+    'import/order': 'off'
   }
 };
